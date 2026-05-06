a353cf08655b9ab0a48bed7ebe927ae8b1a01955
killioin007-source <killioin.007@gmail.com>
2026-05-06 13:46:34 -0400

    added more code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(NameFormatter.format("Dr. Utsav Bahadur Thapa III"));
 
+        System.out.println(NameFormatter.format("Dr","Utsav","Thapa"));
+
+        System.out.println(NameFormatter.format("Utsav","Thapa"));
+
+        System.out.println(NameFormatter.format("Utsav","Thapa","III"));
     }
 }
--- src/main/java/NameFormatter.java
+++ src/main/java/NameFormatter.java
@@ -1,20 +1,37 @@
 public class NameFormatter {
-    private String prefix;
-    private String firstName;
-    private String middleName;
-    private String lastName;
-    private String suffix;
+    private NameFormatter() {
+      }
 
-    public static String format( String firstname, String lastname) {
-    return firstname + " " + lastname;
+    public static String format(String firstname, String lastname) {
+    return lastname + ", " + firstname;
     }
 
     public static String format(String prefix, String firstName, String middleName,String lastName, String suffix) {
-        return pre
+        return lastName + ", " + prefix + " " + firstName + " " + middleName + ", " + suffix;
 
+    }
+    //TODO: wrong logic used (how does it know if itsnt firstname, lastname and suffix)
+    public static String format(String prefix, String firstName, String lastName) {
+        if (prefix.endsWith(".")) {
+            return lastName + ", " + prefix + " " + firstName;
+        } else {
+            // its firstname, lastname  and suffix
+            return firstName + ", " + prefix + " " + lastName /*ACTUALLY A SUFFIX NOT A PREFIX */;
+            }
+        //todo: what if its in firstname, middle name, lastname format
+    }
+    public static String format(String prefix, String firstName, String middleName, String lastName) {
+        return lastName + ", " + prefix + " " + firstName + " " + middleName;
     }
 
     public static String format(String fullName) {
+        String[] nameParts = fullName.split(" ");
 
+        String prefix = nameParts[0];
+        String firstName = nameParts[1];
+        String middleName = nameParts[2];
+        String lastName = nameParts[3];
+        String suffix = nameParts[4];
+        return lastName + ", " + prefix + " " + firstName + " " + middleName + ", " + suffix;
     }
 }
