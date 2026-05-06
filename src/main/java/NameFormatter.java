ee03cdf0945ca73506e53ba68bcaf9b60fd1f159
killioin007-source <killioin.007@gmail.com>
2026-05-06 10:00:57 -0400

    initial commit

---
+++ src/main/java/NameFormatter.java
@@ -0,0 +1,20 @@
+public class NameFormatter {
+    private String prefix;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String suffix;
+
+    public static String format( String firstname, String lastname) {
+    return firstname + " " + lastname;
+    }
+
+    public static String format(String prefix, String firstName, String middleName,String lastName, String suffix) {
+        return pre
+
+    }
+
+    public static String format(String fullName) {
+
+    }
+}
